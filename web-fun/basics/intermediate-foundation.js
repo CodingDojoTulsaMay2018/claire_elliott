36563334687b48822795fb7ce267803abafbf3fa
Claire Elliott <yue.katou@gmail.com>
2018-05-22 08:58:26 -0500

    tuesday may 22-18

--- web-fun/basics/intermediate-foundation.js
+++ web-fun/basics/intermediate-foundation.js
@@ -20,13 +20,11 @@ function factorial(num){
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  Do this without using recursion first.  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment.
 
 function fibonacci(num){
-    arr=[0,1,1]
+    arr=[0,1];
     for(i = 2; i <= num; i++){
         arr[i] = arr[i-1] + arr[i-2];
-        arr.push(arr[i]);
     }
-    arr.pop()
-    console.log(arr)
+    return arr;
 }
 
 // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
---
+++ web-fun/basics/playing-with-objects.js
@@ -0,0 +1,5 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+for(i=0; i < users.length; i++){
+    console.log(users[i].name+" - "+users[i].age);
+}
