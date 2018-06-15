33b8483e5b173e7ac326514195defee35d37cfa5
Claire Elliott <yue.katou@gmail.com>
2018-06-15 15:00:34 -0500

    Dojo Ninjas added

---
+++ python-stack/django/multiple_app/apps/dojo_ninjas/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)
