cb6c0cb253bfaa2f8d15090d7e6eefb9b4b15b06
Claire Elliott <yue.katou@gmail.com>
2018-06-15 15:01:23 -0500

    User Login added

---
+++ python-stack/django/multiple_app/apps/user_logins/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserLoginsConfig(AppConfig):
+    name = 'user_logins'
---
+++ python-stack/django/multiple_app/apps/user_logins/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.EmailField(max_length=255, unique=True)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)
