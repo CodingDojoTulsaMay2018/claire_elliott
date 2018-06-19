97afdf8d7a21ad2fa0802ff00efc61744d76f5c5
Claire Elliott <yue.katou@gmail.com>
2018-06-19 14:27:37 -0500

    Tues 6-19-18

--- python-stack/django/assignments/assignments/urls.py
+++ python-stack/django/assignments/assignments/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^courses/', include('apps.courses.urls')),
-    url(r'^amadon/', include('apps.amadon.urls'))
+    url(r'^amadon/', include('apps.amadon.urls')),
+    url(r'^dashboard/', include('apps.login_registration.urls'))
 ]
