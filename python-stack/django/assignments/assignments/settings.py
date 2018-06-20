a1ae030a97f396ee2cf0ab5891d9c2c93d2bc1e2
Claire Elliott <yue.katou@gmail.com>
2018-06-20 08:51:37 -0500

    Wed 6-20-18 updates

--- python-stack/django/assignments/assignments/settings.py
+++ python-stack/django/assignments/assignments/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'apps.courses',
     'apps.amadon',
     'apps.login_registration',
+    'apps.the_wall',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- python-stack/django/assignments/assignments/urls.py
+++ python-stack/django/assignments/assignments/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 
 urlpatterns = [
     url(r'^courses/', include('apps.courses.urls')),
     url(r'^amadon/', include('apps.amadon.urls')),
-    url(r'^dashboard/', include('apps.login_registration.urls'))
+    url(r'^dashboard/', include('apps.login_registration.urls')),
+    url(r'^wall/', include('apps.the_wall.urls')),
 ]
