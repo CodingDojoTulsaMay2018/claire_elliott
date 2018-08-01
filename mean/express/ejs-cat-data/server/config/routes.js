d540111261675f8a5f1ee3f7b8395ad1d8d29fa0
Claire Elliott <yue.katou@gmail.com>
2018-08-01 18:59:33 -0500

    Cat Data updated

--- mean/express/ejs-cat-data/server/config/routes.js
+++ mean/express/ejs-cat-data/server/config/routes.js
@@ -5,41 +5,45 @@ module.exports = app =>{
 
     app.get("/cats/1", (req,res)=>{
         var cat_info = {
+            imgurl: "/images/husky01.jpg",
             name: "Pickles",
             favthing: "Licking windows",
             age:4,
             interests: ["eating toilet paper","taking baths","WALKIES!!","head pats"]
         };
-        res.render("profile1",{cat: cat_info});
+        res.render("profile",{cat: cat_info});
     });
 
     app.get("/cats/2", (req,res)=>{
         var cat_info = {
+            imgurl: "/images/husky02.jpg",
             name: "Smuggy McSmuggerton",
             favthing: "Judging you",
             age:6,
             interests: ["not fetching that ball","sitting on your bed","pets"]
         };
-        res.render("profile2",{cat: cat_info});
+        res.render("profile",{cat: cat_info});
     });
 
     app.get("/cats/3", (req,res)=>{
         var cat_info = {
+            imgurl: "/images/husky03.jpg",
             name: "Rascal",
             favthing: "Making snow angels",
             age:1,
             interests: ["taking long walks","hiding in the snow","hunting woodland creatures","playing fetch"]
         };
-        res.render("profile3",{cat: cat_info});
+        res.render("profile",{cat: cat_info});
     });
 
     app.get("/cats/4", (req,res)=>{
         var cat_info = {
+            imgurl: "/images/husky04.jpg",
             name: "Simba",
             favthing: "Being held",
             age: "2 months",
             interests: ["screaming a lot","especially at night","hiding under blankets","seeking revenge for my murdered father"]
         };
-        res.render("profile4",{cat: cat_info});
+        res.render("profile",{cat: cat_info});
     });
 };
