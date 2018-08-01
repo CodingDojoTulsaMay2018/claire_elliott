fc0ea35c750fa5aa1857466c5143e635d173122b
Claire Elliott <yue.katou@gmail.com>
2018-08-01 16:26:24 -0500

    Cats and Cars added

---
+++ mean/node/cars-and-cats/app.js
@@ -0,0 +1,112 @@
+// get the http module:
+var http = require('http');
+var path = require("path");
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/acura_nsx.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/acura_nsx.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/pontiac_firebird.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/pontiac_firebird.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/pontiac_gto.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/pontiac_gto.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/toyota_avalon.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/toyota_avalon.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/cats') {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/husky01.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/husky01.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/husky02.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/husky02.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/husky03.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/husky03.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/husky04.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/husky04.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('views/newcar.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-type': 'text/css'});
+         response.write(contents);
+         response.end();
+        });
+    }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 7077");
