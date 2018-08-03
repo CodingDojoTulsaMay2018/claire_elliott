4db78b69b059e5f1cac8f063300d64469a48ea69
Claire Elliott <yue.katou@gmail.com>
2018-08-03 11:59:45 -0500

    Survey Form Revisited updated

--- mean/express/survey-form2/server.js
+++ mean/express/survey-form2/server.js
@@ -9,9 +9,17 @@ app.set("view engine", "ejs");
 require("./server/config/routes")(app);
 
 io.on('connection', function (socket){
-    console.log("I made it heare!");
+    console.log("I made it here!");
     socket.on('posting_form', function(data){
+        console.log("I've received the data!");
         let luckyNum = Math.floor(Math.random()*1000)+1;
-        socket.emit('updated_message', {msg: `You emitted the following information to the server: ${data}\n\nYour lucky number emitted by the server is ${luckyNum}!`});
-    } );
+        socket.emit('updated_message', {msg: `You emitted the following information to the server: { 
+            first name: ${data.answers.fname}, 
+            last name: ${data.answers.lname}, 
+            meaning: ${data.answers.meaning}, 
+            toxin: ${data.answers.toxin}, 
+            deaths: ${data.answers.deaths}, 
+            country: ${data.answers.country} }
+            \n\nYour lucky number emitted by the server is ${luckyNum}!`});
+    });
 });
